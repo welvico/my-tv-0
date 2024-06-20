c676c69eef72fa5e5b433820db1774ee36666ec8
welvico <veryok-abc@yahoo.com>
2024-06-20 15:30:57 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,16 @@
-import java.io.BufferedReader
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android") version "1.6.10"
 }
 
 android {
-    namespace = "com.lizongying.mytv0"
-    compileSdk = 34
-
+    compileSdk = 32
     defaultConfig {
         applicationId = "com.lizongying.mytv0"
         minSdk = 19
-        targetSdk = 34
+        targetSdk = 32
         versionCode = getVersionCode()
         versionName = getVersionName()
         multiDexEnabled = true
@@ -20,6 +18,15 @@ android {
 
     buildFeatures {
         viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 
     buildTypes {
@@ -31,17 +38,40 @@ android {
             )
         }
     }
-    compileOptions {
-        // Flag to enable support for the new language APIs
 
-        // For AGP 4.1+
-        isCoreLibraryDesugaringEnabled = true
-
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    dependencies {
+        implementation("androidx.core:core-ktx:1.7.0")
+        implementation("androidx.appcompat:appcompat:1.4.1")
+        implementation("androidx.multidex:multidex:2.0.1")
+        implementation("androidx.recyclerview:recyclerview:1.3.2")
+        
+        // media3 dependencies
+        val media3Version = "1.3.1"
+        implementation("androidx.media3:media3-ui:$media3Version")
+        implementation("androidx.media3:media3-exoplayer:$media3Version")
+        implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
+        implementation("androidx.media3:media3-exoplayer-dash:$media3Version")
+        implementation("androidx.media3:media3-exoplayer-rtsp:$media3Version")
+        
+        // Other dependencies
+        implementation("com.github.bumptech.glide:glide:4.16.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+        implementation("com.squareup.retrofit2:retrofit:2.11.0")
+        implementation("com.google.android.exoplayer:exoplayer-ui:2.13.3")
+        implementation("com.google.android.exoplayer:exoplayer-core:2.13.3")
+        implementation("com.google.android.exoplayer:exoplayer-hls:2.13.3")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+        implementation(files("libs/lib-decoder-ffmpeg-release.aar"))
+        implementation("io.github.lizongying:gua64:1.4.5")
+        implementation("org.nanohttpd:nanohttpd:2.3.1")
+        implementation("com.google.zxing:core:3.5.3")
+        implementation("androidx.leanback:leanback:1.0.0")
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
     }
 }
 
@@ -70,51 +100,4 @@ fun getVersionName(): String {
     } catch (ignored: Exception) {
         "1.0.0"
     }
-}
-
-dependencies {
-    // For AGP 7.4+
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-
-    // 19 java8
-    val media3Version = "1.3.1"
-    implementation("androidx.media3:media3-ui:$media3Version")
-
-    // For media playback using ExoPlayer
-    implementation("androidx.media3:media3-exoplayer:$media3Version")
-
-    implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
-    implementation("androidx.media3:media3-exoplayer-dash:$media3Version")
-    implementation("androidx.media3:media3-exoplayer-rtsp:$media3Version")
-
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.multidex:multidex:2.0.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-
-    //java7
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-
-    // 21:2.11.0 17:2.6.4
-    val retrofit2Version = "2.11.0"
-    implementation("com.squareup.retrofit2:converter-gson:$retrofit2Version")
-    implementation ("com.squareup.retrofit2:retrofit:$retrofit2Version")
-
-    // For yunos
-    val exoplayerVersion = "2.13.3"
-    implementation("com.google.android.exoplayer:exoplayer-ui:$exoplayerVersion")
-    implementation("com.google.android.exoplayer:exoplayer-core:$exoplayerVersion")
-    implementation("com.google.android.exoplayer:exoplayer-hls:$exoplayerVersion")
-
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-
-    implementation(files("libs/lib-decoder-ffmpeg-release.aar"))
-
-    implementation("io.github.lizongying:gua64:1.4.5")
-
-    implementation("org.nanohttpd:nanohttpd:2.3.1")
-
-    implementation("com.google.zxing:core:3.5.3")
-
-    implementation("androidx.leanback:leanback:1.0.0")
 }
